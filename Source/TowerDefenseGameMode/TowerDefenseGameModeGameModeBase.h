2bb2cfd552c199a23e22d7569b79dfa32b70022f
yongstar98 <68491396+yongstar98@users.noreply.github.com>
2024-08-05 10:54:19 +0900

    0805

---
+++ Source/TowerDefenseGameMode/TowerDefenseGameModeGameModeBase.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TowerDefenseGameModeGameModeBase.generated.h"
+
+UCLASS()
+class TOWERDEFENSEGAMEMODE_API ATowerDefenseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	ATowerDefenseGameMode();
+};
